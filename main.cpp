13b2f3a6ccbc63e36111316b077ec4a65f25df2c
pytton <pytton@hotmail.com>
2016-05-05 15:39:42 -0500

    first changes
    altered main.cpp, second file.cpp unchanged

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
+new idea - adding code before the initial first file
+
 first source code file
 
 changing the first source code file after adding second source code file
-second source code unaltered
+second source code changing unaltered to unchanged - part of new idea
+
